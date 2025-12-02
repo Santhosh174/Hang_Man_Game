4e5a7dfc8fd491c8a9ebeddbd452b2262284d48c
Santhosh Edamalai <santhoshe@m-s.in>
2025-12-02 14:38:09 +0530

    ADDED: message and drawing fields

---
+++ HangMan/Data/Drawing.cs
@@ -0,0 +1,15 @@
+﻿namespace HangMan.Data
+{
+	internal class Drawing
+	{
+		#region Fields
+		public static string strFirstChance = "Wrong guesses: 0\r\n--------------------\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|____________________\r\n";
+		public static string strSecondChance = "Wrong guesses: 1\r\n--------------------\r\n|          O\r\n|\r\n|\r\n|\r\n|\r\n|____________________\r\n";
+		public static string strThirdChance = "Wrong guesses: 2\r\n--------------------\r\n|          O\r\n|          |\r\n|          |\r\n|\r\n|\r\n|____________________\r\n";
+		public static string strFourthChance = "Wrong guesses: 3\r\n--------------------\r\n|          O\r\n|         /|\r\n|          |\r\n|\r\n|\r\n|____________________\r\n";
+		public static string strFifthChance = "Wrong guesses: 4\r\n--------------------\r\n|          O\r\n|         /|\\\r\n|          |\r\n|\r\n|\r\n|____________________\r\n";
+		public static string strSixthChance = "Wrong guesses: 5\r\n--------------------\r\n|          O\r\n|         /|\\\r\n|          |\r\n|         /\r\n|\r\n|____________________\r\n";
+		public static string strSeventhChance = "Wrong guesses: 6\r\n--------------------\r\n|          O\r\n|         /|\\\r\n|          |\r\n|         / \\\r\n|\r\n|____________________\r\n";
+		#endregion
+	}
+}
---
+++ HangMan/Data/Message.cs
@@ -0,0 +1,21 @@
+﻿namespace HangMan.Data
+{
+	internal class Message
+	{
+		#region Fields
+		public static string strWelcome = "Welcome to Hangman!\n";
+		public static string strWonMessage = "Congratulations! You've won!";
+		public static string strLostMessage = "Sorry, you've lost. Better luck next time!";
+		#endregion
+
+		#region Properties
+		public static string strChanceMessage { get; set; }
+		#endregion
+
+		#region Publics
+		public void CheckRemainingChances()
+		{
+		}
+		#endregion
+	}
+}
